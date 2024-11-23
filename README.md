9e498cf13d4d83c1ab88acfeda43f005c70a6169
munene-m <munenenjue18@gmail.com>
2024-11-23 22:49:24 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Deno x Hono Notes API
+
+Simple notes api that saves notes to json file
+
+- Install dependencies
+
+```
+deno install
+```
+
+- To start the server run the following command
+
+```
+deno task dev
+```
+
+- To create a note make a POST request to `http://localhost:8080/create-note` with the following req body:
+
+```
+{
+    "title": "Example title",
+    "content": "Example content"
+}
+```
+
+- To get all notes make a GET request to `http://localhost:8080/notes`
+
+- To get a single note make a GET request to `http://localhost:8080/notes/:id`
---
+++ main.ts
@@ -0,0 +1,95 @@
+import { Hono, type Context } from "@hono/hono";
+
+const app = new Hono();
+
+interface Note {
+  id?: string;
+  title: string;
+  description: string;
+}
+function validateNote(note: any): note is Note {
+  return (
+    typeof note === "object" &&
+    note !== null &&
+    typeof note.title === "string" &&
+    typeof note.content === "string" &&
+    note.title.trim().length > 0 &&
+    note.content.trim().length > 0
+  );
+}
+
+const filePath = "./notes.json";
+
+async function saveToFile(note: Note): Promise<void> {
+  try {
+    let notes: Note[] = [];
+
+    try {
+      const fileContent = await Deno.readTextFile(filePath);
+      notes = JSON.parse(fileContent);
+    } catch (error) {
+      if (error instanceof Deno.errors.NotFound) {
+        console.log("File does not exist, creating new one...");
+        await Deno.writeTextFile(filePath, JSON.stringify([], null, 2));
+      } else {
+        throw error;
+      }
+    }
+    notes.push(note);
+    await Deno.writeTextFile(filePath, JSON.stringify(notes, null, 2));
+    console.log("new note saved to file");
+  } catch (err) {
+    console.error("Error saving note to file:", err);
+    throw err;
+  }
+}
+
+app.get("/", (c: Context) => {
+  return c.text("Welcome to not3s");
+});
+
+app.post("/create-note", async (c: Context) => {
+  try {
+    const body = await c.req.json();
+    if (!validateNote(body)) {
+      return c.json({ error: "Invalid request body" }, 400);
+    }
+    const note: Note = body;
+    note.id = crypto.randomUUID();
+    await saveToFile(note);
+
+    console.log({ message: "Note saved successfully", note }, 201);
+    return c.json({ message: "Note created successfully", note }, 201);
+  } catch (error) {
+    console.error("Error creating note:", error);
+    return c.json({ error: "Invalid JSON or bad request" }, 400);
+  }
+});
+
+app.get("/notes", async (c: Context) => {
+  try {
+    const notes = await Deno.readTextFile(filePath);
+    const parsedNotes: Note[] = JSON.parse(notes);
+    return c.json(parsedNotes);
+  } catch (err) {
+    console.error("Error fetching notes:", err);
+    return c.json({ error: "Failed to fetch notes" }, 500);
+  }
+});
+
+app.get("/notes/:id", async (c: Context) => {
+  const id = c.req.param("id");
+  try {
+    const notes = await Deno.readTextFile(filePath);
+    const parsedNotes: Note[] = JSON.parse(notes);
+    const note = parsedNotes.find((n) => n.id === id);
+    if (!note) {
+      return c.json({ error: "Note not found" }, 404);
+    }
+    return c.json(note);
+  } catch (err) {
+    console.error("Error fetching note:", err);
+    return c.json({ error: "Failed to fetch note" }, 500);
+  }
+});
+Deno.serve(app.fetch);
